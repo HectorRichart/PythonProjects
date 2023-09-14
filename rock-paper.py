2f86982f7e7946cc6e5e3f16e040f6965250f8fe
Hector Richart <hrrichartru@miners.utep.edu>
2023-09-13 20:11:54 -0600

    test

--- rock-paper.py
+++ rock-paper.py
@@ -1,28 +1,40 @@
 import random
-import tkinter as tk
-
-window = tk.Tk()
-window.title("Python GUI")
 
 options = ['R', 'P', 'S']
 computer = random.choice(options)
 user_choice = input("Make a choice: (R,P,S)\n")
 
 if user_choice.upper() == 'R' and computer == 'R':
-    print("Tie")
+    print("Tiee")
+    print(computer)
 elif user_choice.upper() == "P" and computer == "P":
     print("Tie")
+    print(computer)
+
 elif user_choice.upper() == "S" and computer == "S":
     print("Tie")
+    print(computer)
+
 elif user_choice.upper() == "R" and computer == "P":
     print("Computer wins")
+    print(computer)
+
 elif user_choice.upper() == "R" and computer == "S":
     print("User wins")
+    print(computer)
+
 elif user_choice.upper() == "P" and computer == "R":
     print("User wins")
+    print(computer)
+
 elif user_choice.upper() == "P" and computer == "S":
     print("Computer Wins")
+    print(computer)
+
 elif user_choice.upper() == "S" and computer == "P":
     print("Computer wins")
+    print(computer)
+
 elif user_choice.upper() == "S" and computer == "R":    
     print("Computer wins")
+    print(computer)
